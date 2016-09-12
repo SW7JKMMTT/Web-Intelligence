0775cb05c5696acc36f30c9f03cbb297daf20efc
Kasper Helsted <khelst13@student.aau.dk>
2016-09-12 12:41:25 +0200

    init splitter

--- kasper_lecture2/src/app.py
+++ kasper_lecture2/src/app.py
@@ -13,4 +13,12 @@ robot_data = requests.get(
 )
 
 for line in robot_data.content.decode(robot_data.encoding).split("\n"):
+    if line[0] == "#":
+        # ignore dis shit, dis is comment
+        continue
+
+    if "#" in line:
+        before_comment = line.split("#")
+        line = before_comment[0]
+
     print(line)
