e2898f069623fbc9ce6e5b5332e2f07f04b146d6
Mathias Sass Michno <mmichn13@student.aau.dk>
2016-09-26 16:44:59 +0200

    hello

--- jesper/indexer.py
+++ jesper/indexer.py
@@ -1,29 +1,68 @@
 import glob
 import begin
+import re
 from bs4 import BeautifulSoup
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from collections import Counter
+from pprint import pprint
+
+not_word_chars = re.compile(r'[^\w]')
+lemma = WordNetLemmatizer()
+inverted_index = dict()
+
+def tokenize_string(string):
+    string = re.sub(not_word_chars, ' ', string).lower()
+    tokens = nltk.word_tokenize(string)
+    tokens = [lemma.lemmatize(token) for token in tokens if token not in stopwords.words('english')]
+    return tokens
+
+def search(query):
+    print(query)
+    findings = list()
+    for word in tokenize_string(query):
+        print(word)
+        if word in inverted_index.keys():
+            found = set([page for page, _ in inverted_index[word]])
+            print('Found:', len(found), 'for', word)
+        else:
+            found = set()
+        findings.append(found)
+
+    for page in set.intersection(*findings):
+        print(page.path)
+
+class Document(object):
+    def __init__(self, path, content):
+        self.path = path
+        soup = BeautifulSoup(content, 'html.parser')
+        for unwanted in soup(['script', 'style']):
+            unwanted.extract()
+        self.tokens = tokenize_string(soup.get_text(strip=True))
+        for token, cnt in Counter(self.tokens).items():
+            if not token in inverted_index.keys():
+                inverted_index[token] = list()
+            inverted_index[token].append((self, cnt))
 
 @begin.start
 def main(dir: 'Directory tree of hosts' = None):
     "Index stuff"
-    print(dir)
-    pass
+    print('Indexing...')
+    cnt = 0
+    for content_file in glob.iglob(dir + '/**/content', recursive=True):
+        if cnt > 20:
+            break
+        with open(content_file, "r") as f:
+            content = bytes(f.read(), 'utf-8')
+            Document(content_file, content.decode('unicode_escape'))
+        cnt += 1
 
-@begin.subcommand
-def test_doc(path):
-    with open(path, "r") as f:
-        Document(path, f.read())
+    print('Done indexing!')
+    while True:
+        query = input('Enter search query: ')
+        if len(query):
+            search(query)
+        else:
+            break
 
-class Document:
-    @begin.subcommand
-    def __init__(self, path, content):
-        self.path = path
-        self.content = content
-        soup = BeautifulSoup(self.content, 'html.parser')
-        for unwanted in soup(['script', 'style']):
-            unwanted.extract()        
-        self.clean_content = soup.get_text()
-        lines = (line.strip() for line in text.splitlines())
-        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-        self.clean_content = '\n'.join(chunk for chunk in chunks if chunk)
-
-        print(self.clean_content)
